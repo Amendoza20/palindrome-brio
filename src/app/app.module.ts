50960df47264164a1b08434fc26c11e5bc3e7419
Amendoza20 <20.angel.mendoza@gmail.com>
2020-03-18 16:51:25 -0400

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftSideComponent } from './left-side/left-side.component';
+import { RightSideComponent } from './right-side/right-side.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
