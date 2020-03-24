411fcd72798a00f69238e1a64dda90f6af77cad0
Amendoza20 <20.angel.mendoza@gmail.com>
2020-03-23 20:16:38 -0400

    final product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { LeftSideComponent } from './left-side/left-side.component';
-import { RightSideComponent } from './right-side/right-side.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -15,8 +12,6 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    LeftSideComponent,
-    RightSideComponent,
     ContentComponent,
     FooterComponent
   ],
