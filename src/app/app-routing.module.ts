d79f5f2b955ffe5993ca6f767ba2fe1d4c812763
Amendoza20 <20.angel.mendoza@gmail.com>
2020-04-02 17:29:08 -0400

    fixes to login as well as app routing from home page to login page. Work needed for CSS on both home page and login form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content.component'
 import { AppComponent } from './app.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent }
+  { path: '', component: ContentComponent },
+  { path:'login', component: NewUserComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
