91f32483d74a04a75ef1e50793514c3764e3e780
Amendoza20 <20.angel.mendoza@gmail.com>
2020-04-12 21:32:25 -0400

    set up form group logic

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
@@ -7,10 +9,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./new-user.component.css']
 })
 export class NewUserComponent implements OnInit {
+  newUserForm: FormGroup;
+  user: User;
 
-  constructor(private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router){
+    this.user = new User;
+
+    this.newUserForm = this.formBuilder.group(
+      {
+        firstName:'',
+        lastName:'',
+        username:'',
+        email:'',
+        reEnterEmail:'',
+        password:'',
+        reEnterPassword:'',
+       }
+    );
+   }
+  
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    this.user.firstName = this.newUserForm.get('firstName').value;
+    this.user.lastName = this.newUserForm.get('lastName').value;
+    this.user.username = this.newUserForm.get('username').value;
+    this.user.email = this.newUserForm.get('email').value;
+    this.user.password= this.newUserForm.get('password').value;
+  }
 }
