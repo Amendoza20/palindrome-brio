56496e383e77ea5285de5cc591075284c4d30bd7
Amendoza20 <20.angel.mendoza@gmail.com>
2020-04-13 14:37:29 -0400

    added validationa for forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Login } from '../models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  login: Login;
 
-  constructor(private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router){
+  
+    this.login = new Login;
+
+    this.loginForm = this.formBuilder.group(
+      {
+        username:['', Validators.required],
+        password:['', Validators.required]
+      }
+    );
+   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    this.login.username = this.loginForm.get('username').value;
+    this.login.password = this.loginForm.get('password').value;
+  }
 }
--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
@@ -18,28 +18,40 @@ export class NewUserComponent implements OnInit {
 
     this.newUserForm = this.formBuilder.group(
       {
-        firstName:'',
-        lastName:'',
-        username:'',
-        email:'',
-        reEnterEmail:'',
-        password:'',
-        reEnterPassword:'',
+        firstName:['', Validators.required],
+        lastName:['', Validators.required],
+        username:['', Validators.required],
+        email:['', Validators.required, Validators.email],
+        reEnterEmail:['', Validators.required],
+        password:['', Validators.required, Validators.minLength, Validators.maxLength],
+        reEnterPassword:['', Validators.required]
        }
     );
-
-    
-
-   }
+    }
   
-
   ngOnInit(): void {
   }
+  
+  passwordValidation(){
+    const { value: password } = this.newUserForm.get('password');
+    const { value: reEnterPassword } = this.newUserForm.get('reEnterPassword');
+    return password === reEnterPassword ? null : { passwordNotMatch: true};
+  } 
+
+  emailValidation(){
+    const { value : email } = this.newUserForm.get('email');
+    const { value: reEnterEmail } = this.newUserForm.get('reEnterEmail');
+    return email === reEnterEmail ? null: { emailNotMathc: true };
+  }
+
   onSubmit(){
+    if (this.passwordValidation() && this.emailValidation()){
     this.user.firstName = this.newUserForm.get('firstName').value;
     this.user.lastName = this.newUserForm.get('lastName').value;
     this.user.username = this.newUserForm.get('username').value;
     this.user.email = this.newUserForm.get('email').value;
-    this.user.password= this.newUserForm.get('password').value;
+    this.user.password = this.newUserForm. get('password').value;
+    //do the thing 
+    }
   }
 }
